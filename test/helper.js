310e02f597fd1a228c560351b7b800e7f3400a2e
sinsoku <sinsoku.listy@gmail.com>
2016-01-27 02:44:59 +0900

    Extract a json reporter

--- test/helper.js
+++ test/helper.js
@@ -2,9 +2,9 @@ global.assert = require('power-assert');
 
 global.Clairvoyance = require('../lib/clairvoyance');
 global.explorer = require('../lib/explorer');
-global.cli = require('../lib/cli');
 global.CssFile = require('../lib/cssfile');
 global.HtmlFile = require('../lib/htmlfile');
+global.jsonReporter = require('../lib/reporters/json');
 
 global.eq = function(value) {
   return function(x) { return x === value; };
