0ed4fedc181813962b8f6b2ed7a515d71f25321b
sinsoku <sinsoku.listy@gmail.com>
2016-05-22 15:44:31 +0900

    Translate 'lib/htmlfile.js' to ES2015

--- src/lib/htmlfile.js
+++ src/lib/htmlfile.js
@@ -1,47 +1,44 @@
-module.exports = HtmlFile;
-
 /**
  * Initialize a new HTML file with the given `path`.
  * @constructor
  * @param {String} path - a file path
  */
-function HtmlFile(path) {
-  this.path = path;
-}
+class HtmlFile {
+  constructor(path) {
+    this.path = path;
+  }
 
-HtmlFile.prototype.querySelectorAll = function(ph, style, cb) {
-  var _this = this;
-  return new Promise(function(resolve) {
-    ph.createPage().then(function(page) {
-      page.open(_this.path).then(function() {
-        var func = 'function() { ' +
-          'return document.querySelectorAll("' + style + '"); }';
-        page.evaluateJavaScript(func).then(function(value) {
-          cb(value);
-          resolve();
+  querySelectorAll(ph, style, cb) {
+    return new Promise(resolve => {
+      ph.createPage().then(page => {
+        page.open(this.path).then(() => {
+          const func = `function() { return document.querySelectorAll("${style}"); }`;
+          page.evaluateJavaScript(func).then(value => {
+            cb(value);
+            resolve();
+          });
         });
       });
     });
-  });
-};
+  }
 
-HtmlFile.prototype.findCss = function(ph, css) {
-  var _this = this;
-  var tasks = [];
+  findCss(ph, css) {
+    const tasks = [];
 
-  css.rules.forEach(function(rule) {
-    if (rule.type !== 'rule') {
-      return;
-    }
+    css.rules.forEach(rule => {
+      if (rule.type !== 'rule') {
+        return;
+      }
 
-    var style = rule.selectors.toString();
-    var task = _this.querySelectorAll(ph, style, function(value) {
-      css.mark(value.length, rule.position.start.line, rule.position.end.line);
+      const style = rule.selectors.toString();
+      const task = this.querySelectorAll(ph, style, value => {
+        css.mark(value.length, rule.position.start.line, rule.position.end.line);
+      });
+      tasks.push(task);
     });
-    tasks.push(task);
-  });
 
-  return new Promise(function(resolve) {
-    Promise.all(tasks).then(resolve);
-  });
-};
+    return new Promise(resolve => Promise.all(tasks).then(resolve));
+  }
+}
+
+module.exports = HtmlFile;
